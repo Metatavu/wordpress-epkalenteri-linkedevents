cd9fb735e708e6495bfcc66a317ce20f5f4df62b
Antti Leppä <antti.leppa@metatavu.fi>
2020-01-15 09:47:42 +0200

    Update wordpress-epkalenteri-linkedevents.php

--- wordpress-epkalenteri-linkedevents.php
+++ wordpress-epkalenteri-linkedevents.php
@@ -3,7 +3,7 @@
  * Created on Sep 18, 2018
  * Plugin Name: Linked Events epkalenteri.fi
  * Description: Wordpress plugin for synchronizing epkalenteri.fi events into Linked Events
- * Version: 2.1.0
+ * Version: 1.1.0
  * Author: Metatavu Oy
  */
 
